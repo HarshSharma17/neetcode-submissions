19f082570c23b5e236878aa72e558d17c1af9cfd
Harsh Sharma <146095128+HarshSharma17@users.noreply.github.com>
2026-09-01 18:01:51 +0530

    Add: verifying-an-alien-dictionary - submission-2

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        ordermap = {}
+
+        for i in range(len(order)):
+            ordermap[order[i]] = i
+
+        for i in range(len(words) - 1):
+            for j in range(len(words[i])):
+
+                if j >= len(words[i + 1]):
+                    return False
+
+                if words[i][j] != words[i + 1][j]:
+
+                    currletter = ordermap[words[i][j]]
+                    nextletter = ordermap[words[i+1][j]]
+
+                    if currletter > nextletter:
+                        return False
+                    else:
+                        break
+        return True
+                    
